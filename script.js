a45eacc12ef0a0323c6fbfff96846ab20eeb4845
poorni141 <poornithaarumugam@gmail.com>
2025-12-16 16:12:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,18 @@
-let pnr = "";
+function validateLogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-function login() {
-    let u = document.getElementById("user").value;
-    let p = document.getElementById("pass").value;
-
-    if (u === "admin" && p === "1234") {
+    if (username !== "" && password !== "") {
         window.location.href = "index.html";
     } else {
-        alert("Invalid Login");
+        alert("Please enter username and password");
     }
 }
 
 function reserve() {
-    let name = document.getElementById("name").value;
-    let train = document.getElementById("train").value;
-    let cls = document.getElementById("classType").value;
-
-    pnr = "PNR" + Math.floor(Math.random() * 10000);
-
-    document.getElementById("output").innerHTML =
-        "Reservation Successful<br>" +
-        "Name: " + name + "<br>" +
-        "Train: " + train + "<br>" +
-        "Class: " + cls + "<br>" +
-        "PNR: " + pnr;
+    alert("Ticket Reserved Successfully!");
 }
 
 function cancel() {
-    if (pnr === "") {
-        document.getElementById("output").innerHTML = "No ticket found";
-    } else {
-        document.getElementById("output").innerHTML =
-            "Ticket " + pnr + " cancelled";
-        pnr = "";
-    }
+    alert("Ticket Cancelled!");
 }
